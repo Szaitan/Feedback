2264f45dda0ac29854525c1854490a6f9c7ff634
Michał <m.krepiniewicz@gmail.com>
2023-11-11 16:34:31 +0100

    Warsaw Rev.2 - Form class and implementation, for validation, getting data from form

--- reviews/views.py
+++ reviews/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from .forms import ReviewForm
 
 # Create your views here.
 
 
 def index(request):
     if request.method == "POST":
-        entered_name = request.POST['username']
-        print(entered_name)
-        return redirect('thank_you_page')
-    return render(request, 'reviews/review.html')
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            entered_name = form.cleaned_data
+            print(entered_name["username"])
+            return redirect('thank_you_page')
+    form = ReviewForm()
+    return render(request, 'reviews/review.html', {'form': form})
 
 
 def thank_you(request):
