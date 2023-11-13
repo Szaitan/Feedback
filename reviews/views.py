c060e922dbd5e67cab09996bdb4fbc02b8ad71b9
michal.krepiniewicz <m.krepiniewicz@gmail.com>
2023-11-13 20:37:23 +0100

    Kutno Rev. 06 - TemplateView in more details

--- reviews/views.py
+++ reviews/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .forms import ReviewForm
 from .models import Review
 from django.views import View
+from django.views.generic.base import TemplateView
 # Create your views here.
 
 
 class ReviewView(View):
     def get(self, request):
         form = ReviewForm()
-        return render(request, 'reviews/review.html', {'form': form})
+        data = "This works?"
+        return render(request, 'reviews/review.html', {'form': form,
+                                                       'data': data})
 
     def post(self, request):
         form = ReviewForm(request.POST)
@@ -19,7 +22,9 @@ class ReviewView(View):
             object_to_save = Review(username=entered_data["username"], review_text=entered_data["review_text"],
                                     rating=entered_data["rating"]).save()
             return redirect('thank_you_page')
-        return render(request, 'reviews/review.html', {'form': form})
+        return render(request, 'reviews/review.html', {
+            'form': form,
+        })
 
 
 # def index(request):
@@ -41,6 +46,47 @@ class ReviewView(View):
 #     # Jeśli jest to POST ale nie jest valid to zostaje ta forma przekazana a nie pusta.
 
 
-class ThankYouView(View):
-    def get(self, request):
-        return render(request, "reviews/thank_you_page.html")
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you_page.html"
+    all_reviews = Review.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class ReviewListView(TemplateView):
+    template_name = "reviews/review_list.html"
+
+    def get_context_data(self, **kwargs):
+        all_reviews = Review.objects.all()
+        context = super().get_context_data(**kwargs)
+        context["all_reviews"] = all_reviews
+        return context
+
+
+class DetailedReviewListView(TemplateView):
+    template_name = "reviews/detailed_review_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print(kwargs['data_id'])
+        print(type(kwargs['data_id']))
+        print(type(Review.objects.get(id=kwargs['data_id'])))
+        xyz = Review.objects.get(id=kwargs['data_id'])
+        print(xyz.review_text)
+        context["data"] = xyz
+        # WAŻNE to co przekazujemy do html to dictionary !!! Więc aby dostać się do danych trzeba w temaplacie wezwać
+        # nazwę dictionary a nie context !!! W tym wypadku jest to "data" !!!
+        return context
+
+
+# class DetailedReviewListView(View):
+#     def get(self, request, data_id):
+#         print(data_id)
+#         data = Review.objects.get(id=data_id)
+#         print(data)
+#         return render(request, "reviews/detailed_review_list.html", {
+#             "context": data,
+#         })
+#
