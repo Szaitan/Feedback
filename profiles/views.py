c4e7de520dba2a19ccf369fa4d68f6789fb6e4ff
michal.krepiniewicz <m.krepiniewicz@gmail.com>
2023-11-15 19:38:58 +0100

    Kutno Rev. 09 - Implementation of method to save loaded files

--- profiles/views.py
+++ profiles/views.py
@@ -4,10 +4,16 @@ from django.views import View
 # Create your views here.
 
 
+def store_file(file):
+    with open(f"temp/{file.name}", "wb+") as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
+
+
 class CreateProfileView(View):
     def get(self, request):
         return render(request, "profiles/create_profile.html")
 
     def post(self, request):
-        print(request.FILES["image"])
+        store_file(request.FILES["image"])
         return redirect("/profiles")
